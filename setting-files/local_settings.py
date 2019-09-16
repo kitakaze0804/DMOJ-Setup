87c81f880499977d2a3f351ef75cdd0eee7032e2
kitakaze0804 <ry.kitakaze@gmail.com>
2019-09-16 20:53:34 +0900

    replaced local_settings.py nginx.conf

--- setting-files/local_settings.py
+++ setting-files/local_settings.py
@@ -162,13 +162,13 @@ BAD_MAIL_PROVIDERS = set()
 
 ## Event server.
 # Uncomment to enable live updating.
-#EVENT_DAEMON_USE = True
+EVENT_DAEMON_USE = True
 
 # Uncomment this section to use websocket/daemon.js included in the site.
 #EVENT_DAEMON_POST = '<ws:// URL to post to>'
 
 # If you are using the defaults from the guide, it is this:
-#EVENT_DAEMON_POST = 'ws://127.0.0.1:15101/'
+EVENT_DAEMON_POST = 'ws://127.0.0.1:15101/'
 
 # These are the publicly accessed interface configurations.
 # They should match those used by the script.
@@ -178,9 +178,9 @@ BAD_MAIL_PROVIDERS = set()
 # i.e. the path to /channels/ exposed by the daemon, through whatever proxy setup you have.
 
 # Using our standard nginx configuration, these should be.
-#EVENT_DAEMON_GET = 'ws://<your domain>/event/'
-#EVENT_DAEMON_GET_SSL = 'wss://<your domain>/event/' # Optional
-#EVENT_DAEMON_POLL = '/channels/'
+EVENT_DAEMON_GET = 'ws://host/event/'
+EVENT_DAEMON_GET_SSL = 'wss://host/event/'  # Optional
+EVENT_DAEMON_POLL = '/channels/'
 
 # If you would like to use the AMQP-based event server from <https://github.com/DMOJ/event-server>,
 # uncomment this section instead. This is more involved, and recommended to be done
